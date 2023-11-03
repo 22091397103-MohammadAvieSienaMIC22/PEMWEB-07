bf922ec550a1408f9a0e1b09e195c366d6326d7f
YamaGucci-web <mohammadavie.22103@mhs.unesa.ac.id>
2023-11-03 23:47:15 +0700

    Create script.js
    menambahkan script.js untuk halaman login

---
+++ script.js
@@ -0,0 +1,26 @@
+const loginText = document.querySelector(".title-text .login");
+      const loginForm = document.querySelector("form.login");
+      const loginBtn = document.querySelector("label.login");
+      const signupBtn = document.querySelector("label.signup");
+      const signupLink = document.querySelector("form .signup-link a");
+      signupBtn.onclick = (()=>{
+        loginForm.style.marginLeft = "-50%";
+        loginText.style.marginLeft = "-50%";
+      });
+      loginBtn.onclick = (()=>{
+        loginForm.style.marginLeft = "0%";
+        loginText.style.marginLeft = "0%";
+      });
+      signupLink.onclick = (()=>{
+        signupBtn.click();
+        return false;
+      });
+
+// Membuat variabel navbar dan mengambil document dari class navbar
+let navbar = document.querySelector('.navbar'); 
+
+// membuat navbar active saat diklik 
+menu.onclick = () => {
+    navbar.classList.toggle('active');
+}
+
